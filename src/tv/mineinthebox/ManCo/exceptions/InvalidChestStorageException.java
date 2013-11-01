f6ccd91f40cbb3ccbf8ba1778d9579956ca16e4b
xize <glucassen@live.nl>
2013-11-01 19:37:41 +0100

    added start for the developer api

---
+++ src/tv/mineinthebox/ManCo/exceptions/InvalidChestStorageException.java
@@ -0,0 +1,15 @@
+package tv.mineinthebox.ManCo.exceptions;
+
+public class InvalidChestStorageException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public InvalidChestStorageException(String message) {
+		super(message);
+	}
+	
+
+}
---
+++ src/tv/mineinthebox/ManCo/exceptions/InvalidRareCrateException.java
@@ -0,0 +1,14 @@
+package tv.mineinthebox.ManCo.exceptions;
+
+public class InvalidRareCrateException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public InvalidRareCrateException(String message) {
+		super(message);
+	}
+
+}
