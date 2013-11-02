88ada40c8d966989e2c5f2c1e000f64160609423
xize <glucassen@live.nl>
2013-11-02 17:07:26 +0100

    added ChestHasNoOwnerException for the api

---
+++ src/tv/mineinthebox/ManCo/exceptions/ChestHasNoOwnerException.java
@@ -0,0 +1,15 @@
+package tv.mineinthebox.ManCo.exceptions;
+
+
+public class ChestHasNoOwnerException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ChestHasNoOwnerException(String message) {
+		super(message);
+	}
+
+}
