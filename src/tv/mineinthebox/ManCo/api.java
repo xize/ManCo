77fcfe3d8e1bec6a1984646550bb0918f6f23925
xize <glucassen@live.nl>
2013-11-02 17:06:38 +0100

    added some more implements to the api

--- src/tv/mineinthebox/ManCo/api.java
+++ src/tv/mineinthebox/ManCo/api.java
@@ -14,12 +14,16 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.inventory.ItemStack;
 
+import tv.mineinthebox.ManCo.configuration.configuration;
 import tv.mineinthebox.ManCo.events.cratescheduler;
+import tv.mineinthebox.ManCo.exceptions.ChestHasNoOwnerException;
 import tv.mineinthebox.ManCo.exceptions.InvalidChestStorageException;
 import tv.mineinthebox.ManCo.exceptions.InvalidRareCrateException;
 import tv.mineinthebox.ManCo.exceptions.PlayerOnSlabException;
 import tv.mineinthebox.ManCo.utils.normalCrate;
+import tv.mineinthebox.ManCo.utils.normalCrateList;
 import tv.mineinthebox.ManCo.utils.rareCrate;
+import tv.mineinthebox.ManCo.utils.rareCrateList;
 import tv.mineinthebox.ManCo.utils.util;
 
 public class api implements Listener {
@@ -32,6 +36,44 @@ public class api implements Listener {
 	public static ItemStack[] convertArrayListToItemStackArray(ArrayList<ItemStack> array) {
 		ItemStack[] items = new ItemStack[array.size()];
 		return items;
+	}
+	
+	public static boolean isCrate(Chest chest) {
+		if(normalCrateList.getCrateList.containsValue(chest)) {
+			return true;
+		} else if(normalCrateList.getCrateList2.containsValue(chest)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean isRareCrate(Chest chest) {
+		if(rareCrateList.getCrateList.containsValue(chest)) {
+			return true;
+		} else if(rareCrateList.getCrateList2.containsValue(chest)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static void destroyCrate(Player p) {
+		configuration.clearPlayerCrate(p);
+	}
+	
+	public static void destroyCrate(String name) {
+		configuration.clearPlayerCrate(name);
+	}
+	
+	public static String getCrateOwner(Chest chest) throws ChestHasNoOwnerException {
+		String normal = configuration.getNormalChestOwner(chest);
+		String rare = configuration.getRareChestOwner(chest);
+		if(normal != null) {
+			return normal;
+		} else if(rare != null) {
+			return rare;
+		} else {
+			throw new ChestHasNoOwnerException("[ManCo-API]ChestHasNoOwnerException: this chest has no ManCo ownership!");
+		}
 	}
 	
 	public static void spawnCrate(Location loc, ItemStack[] items) throws PlayerOnSlabException {
