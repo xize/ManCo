1be0b7e8a9d7a9e601b0bdc18d518b3fdb0f83e3
xize <glucassen@live.nl>
2013-11-23 13:55:57 +0100

    added permission manco.ignore for operators and admins to ignore crates

--- src/tv/mineinthebox/ManCo/utils/util.java
+++ src/tv/mineinthebox/ManCo/utils/util.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
 
 import tv.mineinthebox.ManCo.logType;
 import tv.mineinthebox.ManCo.ManCo;
@@ -33,6 +34,13 @@ public class util {
 	public static boolean isSlab(Block block) {
 		Block highest = block.getLocation().getWorld().getHighestBlockAt(block.getLocation());
 		if(highest.getRelative(BlockFace.DOWN).getType() == Material.getMaterial(126) || highest.getRelative(BlockFace.DOWN).getType() == Material.getMaterial(44) || highest.getType() == Material.RAILS || highest.getType() == Material.TORCH || highest.getType() == Material.REDSTONE_WIRE || highest.getType() == Material.REDSTONE_TORCH_ON || highest.getType() == Material.REDSTONE_TORCH_OFF || highest.getType() == Material.ACTIVATOR_RAIL || highest.getType() == Material.ANVIL || highest.getType() == Material.CHEST || highest.getType() == Material.FENCE || highest.getType() == Material.STONE_PLATE || highest.getType() == Material.WOOD_PLATE || highest.getType() == Material.WHEAT || highest.getType() == Material.MELON_STEM || highest.getType() == Material.PUMPKIN_STEM || highest.getType() == Material.RAILS || highest.getType() == Material.TORCH || highest.getType() == Material.REDSTONE_WIRE || highest.getType() == Material.REDSTONE_TORCH_ON || highest.getType() == Material.REDSTONE_TORCH_OFF || highest.getType() == Material.ACTIVATOR_RAIL || highest.getType() == Material.ANVIL || highest.getType() == Material.CHEST || highest.getType() == Material.FENCE || highest.getType() == Material.STONE_PLATE || highest.getType() == Material.TRIPWIRE || highest.getType() == Material.NETHER_FENCE || highest.getType() == Material.RAILS || highest.getType() == Material.TORCH || highest.getType() == Material.REDSTONE_WIRE || highest.getType() == Material.REDSTONE_TORCH_ON || highest.getType() == Material.REDSTONE_TORCH_OFF || highest.getType() == Material.ACTIVATOR_RAIL || highest.getType() == Material.ANVIL || highest.getType() == Material.CHEST || highest.getType() == Material.FENCE || highest.getType() == Material.CROPS || highest.getType() == Material.SIGN_POST || highest.getType() == Material.SUGAR_CANE_BLOCK || highest.getType() == Material.STONE_PLATE) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean isStaff(Player p) {
+		if(p.hasPermission("manco.ignore")) {
 			return true;
 		}
 		return false;
