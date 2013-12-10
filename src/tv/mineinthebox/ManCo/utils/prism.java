a7603514352920fbf20018c87b531142fff90e61
xize <glucassen@live.nl>
2013-12-10 19:58:24 +0100

    fixed prism logging now works properly!

--- src/tv/mineinthebox/ManCo/utils/prism.java
+++ src/tv/mineinthebox/ManCo/utils/prism.java
@@ -1,28 +1,13 @@
 package tv.mineinthebox.ManCo.utils;
 
 import me.botsko.prism.Prism;
-import me.botsko.prism.actionlibs.ActionsQuery;
-import me.botsko.prism.actionlibs.MatchRule;
-import me.botsko.prism.actionlibs.QueryParameters;
+import me.botsko.prism.actionlibs.ActionFactory;
 
-import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 
 public class prism {
-	
-	@SuppressWarnings("deprecation")
 	public static void log(String p, Block block) {
-		Prism prism = (Prism) Bukkit.getPluginManager().getPlugin("Prism");
-		QueryParameters param = new QueryParameters();
-		param.setWorld(block.getWorld().getName());
-		param.addActionType("block-place", MatchRule.INCLUDE);
-		param.setRadius(20);
-        param.setLimit(100);
-        param.addPlayerName(p);
-        param.addSpecificBlockLocation(block.getLocation());
-        param.addBlockFilter(block.getTypeId(), block.getData());
-        ActionsQuery doQuery = new ActionsQuery(prism);
-        doQuery.lookup(param);
+		Prism.actionsRecorder.addToQueue(ActionFactory.create("block-place", block, p));
 	}
 
 }
